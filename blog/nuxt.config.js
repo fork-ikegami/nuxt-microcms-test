bf203673575a7a42597cee8ffec758a4e1bbc6a9
Maki Ikegami <maki.ikegami@fork.co.jp>
2021-11-05 20:05:03 +0900

    Update nuxt.config.js

--- blog/nuxt.config.js
+++ blog/nuxt.config.js
@@ -10,9 +10,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: 'nuxt-microcms-test',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
