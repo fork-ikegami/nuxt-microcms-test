9e140f6681715d93083657064ebb9e3c2212fea9
fork-ikegami <fork-ikegami@users.noreply.github.com>
2021-11-10 11:15:07 +0000

    deploy: bd26e889bc9c6a916dc933e72be242df11e79341

---
+++ _nuxt/static/1636542898/vxvyuywug/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-microcms-test\u002F_nuxt\u002Fstatic\u002F1636542898",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvxvyuywug",config:{_app:{basePath:"\u002Fnuxt-microcms-test\u002F",assetsPath:"\u002Fnuxt-microcms-test\u002F_nuxt\u002F",cdnURL:a}}}}(null));
